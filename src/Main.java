770e83c0b9eaffd7251990e3cbdf43227a093733
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-04-15 13:52:32 -0700

    idk

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
+		//idk
 		List<String> lines = Files.readAllLines(Paths.get("input.txt"));
 		
 		for(int i = 0 ; i<lines.size() ; i++)
