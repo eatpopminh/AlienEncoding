253d6acd61fea157dbcd8014fee61c18b5f2ec7e
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-04-17 14:14:54 -0700

    did some testing around

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,43 @@
 //Due April 23
+import java.util.List;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Vector;
+
+
+
+
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
-		//idk
+		//Use Tree NOT binary tree to get more than two leaf on one root.
 		List<String> lines = Files.readAllLines(Paths.get("input.txt"));
+		int freq = Integer.parseInt(lines.get(0));
 		
-		for(int i = 0 ; i<lines.size() ; i++)
+		TreeMap data = new TreeMap<Integer, String>();
+		
+		for(int i = 1 ; i<lines.size() ; i++)
 		{
+			System.out.println(lines.get(i).split(" ")[0] + " " + Integer.parseInt(lines.get(i).split(" ")[1]));
 			
+			data.put(Integer.parseInt(lines.get(i).split(" ")[1]), lines.get(i).split(" ")[0]);
 		}
+		
+		
+		
+
+		
+	
 		
 	}
 
